1acfbde609ff2fe7ca6fffbf7666ed657e0d33ec
Eric Dasse <christ.gououi@gmail.com>
2022-12-05 13:48:15 +0100

    Remove useless files and mentions
    This is to start the simplest way possible

--- src/domain/ports/extract.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class MusicExtractorService:
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def get_recently_played_tracks(self, number_of_tracks):
-        pass
--- src/infra/spotify_extract.py
+++
@@ -1,6 +0,0 @@
-from domain.ports.extract import MusicExtractorService
-
-
-class SpotifyMusicExtractorService(MusicExtractorService):
-    def get_recently_played_tracks(self):
-        pass
