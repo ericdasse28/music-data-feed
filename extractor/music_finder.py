858e174479895a3e9684ab0b5a54fecb0265a368
Eric Dasse <christ.gououi@gmail.com>
2022-12-18 20:49:22 +0100

    `music_finder.py`: Add type hint

--- extractor/music_finder.py
+++ extractor/music_finder.py
@@ -1,6 +1,6 @@
 class MusicFinder:
-    def __init__(self, music_finding_method):
+    def __init__(self, music_finding_method: function):
         self.music_finding_method = music_finding_method
 
-    def find_music(self, number_of_songs):
+    def find_music(self, number_of_songs: int):
         return self.music_finding_method(number_of_songs)
