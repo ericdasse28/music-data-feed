2674e73b6999be80d25be2ac879cc433e660f0e2
Eric Dasse <christ.gououi@gmail.com>
2022-12-18 20:51:49 +0100

    `find_music`: Add guard clause

--- extractor/music_finder.py
+++ extractor/music_finder.py
@@ -3,4 +3,8 @@ class MusicFinder:
         self.music_finding_method = music_finding_method
 
     def find_music(self, number_of_songs: int):
+        if not isinstance(number_of_songs, int) or number_of_songs < 0:
+            raise AttributeError(
+                "find_music argument should be an integer greater than or equal to 0"
+            )
         return self.music_finding_method(number_of_songs)
